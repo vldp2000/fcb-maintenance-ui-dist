f42dc347c9d3c7d5012bcaff439d8c7985be329d
Vlad Perepelytsya <Vlad.Perepelytsya@leapdev.io>
2026-06-27 23:35:18 +1000

    Release UI 2026-06-27T13:35:18Z (726f654)

--- service-worker.js
+++ service-worker.js
@@ -14,16 +14,14 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7cf34ea8affabcf8c5f6e95b3530adf7.js"
+  "/precache-manifest.d2966f7b662a35ca6acc82e037ddc316.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "maintenance"});
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
