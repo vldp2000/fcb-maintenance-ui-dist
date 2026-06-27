b0f633a20c490c7e26ada68afcb5333b2f978475
Vlad Perepelytsya <Vlad.Perepelytsya@leapdev.io>
2026-06-27 20:24:26 +1000

    Release UI 2026-06-27T10:24:25Z (d9412a8a)

---
+++ precache-manifest.f01f22d5cfab951288aa383deaf641fc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29842a5d136feae74153",
+    "url": "/css/app.3fb79897.css"
+  },
+  {
+    "revision": "3fcabfd67f0eef82a46c",
+    "url": "/css/chunk-vendors.efa13a14.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "05726ce9432b4084936dcbbe68ce9f51",
+    "url": "/img/BiasAmp.05726ce9.png"
+  },
+  {
+    "revision": "3f6122bf62be04166f7b9c3ca7173420",
+    "url": "/img/image_01.3f6122bf.png"
+  },
+  {
+    "revision": "1bec50ba308bb460ee26538d44acd542",
+    "url": "/img/image_02.1bec50ba.png"
+  },
+  {
+    "revision": "0adfd27a26029cf36369b4c27c860e1a",
+    "url": "/img/image_03.0adfd27a.png"
+  },
+  {
+    "revision": "b35e7435919c4d63272b0a37abb69b17",
+    "url": "/img/image_04.b35e7435.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "242f919fccbc4b0df21a455fb22f1a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29842a5d136feae74153",
+    "url": "/js/app.5d9c0c3c.js"
+  },
+  {
+    "revision": "3fcabfd67f0eef82a46c",
+    "url": "/js/chunk-vendors.7737b32f.js"
+  },
+  {
+    "revision": "61484ed16ded45701c5ca5fbc9760066",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
